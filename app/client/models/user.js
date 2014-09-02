74998cce972fb1e2a52b4ad7fc0e5225c622685c
vikram <viksdegod@gmail.com>
2014-09-02 18:38:01 +0530

    Cleaned up unneeded stuff

--- app/client/models/user.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var usersScheme=new usersScheme({
-    name: string
-})
-mongoose.model(users,usersScheme)
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-
-/*
- * GET home page.
- */
-module.exports
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
-};
