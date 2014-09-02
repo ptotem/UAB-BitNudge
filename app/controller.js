dafc43292cfe13e817adbef1f7ab2b2b8786bbc1
vikram <viksdegod@gmail.com>
2014-09-02 18:24:06 +0530

    Moved code to controller fully

--- app/controller.js
+++ app/controller.js
@@ -1,3 +1,46 @@
 var Controller={
+
+  addOrganization:function(Collections,response,userRole,userName,userTeams){
+    Collections.Acl.findOne({collectionName:"Organizations","controlList.role":userRole},function(err,obj){
+      // if(obj.
+      if(err)
+        response.send("You cant add Organizations.");
+      var test=new Collections.Organizations({name:userName,teams:userTeams.split(',')});
+      test.save();
+      response.send("Org added.");
+    });
+  },
+  addTeam:function(Collections,res,userRole,userName,users){
+    Collections.Acl.findOne({collectionName:"Teams","controlList.role":userRole},function(err,obj){
+      // if(obj.
+      if(err)
+        res.send("You cant add TEams.");
+      else{
+        var test=new Collections.Teams({name:userName,users:users.split(',')});
+        test.save();
+        res.send("Team added.");
+      }
+    });
+  },
+  getTeam:function(Collections,res,userRole,teamName){
+    Collections.Acl.findOne({collectionName:"Teams","controlList.role":req.user.role},function(err,obj){
+      if(err)
+      res.send("you cant see this team");
+      else
+      Collections.Teams.findOne({name:teamName},function(err,teamobj){
+        res.send(teamobj);
+      });
+    });
+  },
+  getOrganization:function(Collections,res,userRole,orgName){
+    Collections.Acl.findOne({collectionName:"Organizations","controlList.role":userRole},function(err,obj){
+      if(err)
+      res.send("you cant see this org");
+      else
+      Collections.Organizations.findOne({name:orgName},function(err,teamobj){
+        res.send(teamobj);
+      });
+    });
+  }
 }
 module.exports=Controller;
--- app/routes.js
+++ app/routes.js
@@ -6,60 +6,35 @@ var initializeRoutes=function(server,Collections,passport){
       return;
     }
   };
+
   server.get('/',function(req,res){
     if(req.isAuthenticated())
       res.send('hahaha');
     else 
       res.send('please login first.');
   });
+
   server.post('/organizations',function(req,res){
     checkLoginBlock(req,res);
     if(req.query.name&&req.query.teams)
-      Collections.Acl.findOne({collectionName:"Organizations","controlList.role":req.user.role},function(err,obj){
-        // if(obj.
-        if(err)
-          res.send("You cant add Organizations.");
-        var test=new Collections.Organizations({name:req.query.name,teams:req.query.teams.split(',')});
-        test.save();
-        res.send("Org added.");
-      });
+      res.send(Controller.addOrganization(Collections,res,req.user.role,req.query.name,req.query.teams));
   });
+
   server.post('/teams',function(req,res){
     if(req.query.name&&req.query.users)
-    Collections.Acl.findOne({collectionName:"Teams","controlList.role":req.user.role},function(err,obj){
-      // if(obj.
-      if(err)
-        res.send("You cant add TEams.");
-      else{
-        var test=new Collections.Teams({name:req.query.name,users:req.query.users.split(',')});
-        test.save();
-        res.send("Team added.");
-      }
-    });
+      res.send(Controller.addTeam(Collections,res,req.user.role,req.query.name,req.query.teams));
   });
+
   server.get('/teams/:teamName',function(req,res){
     checkLoginBlock(req,res)
-    Collections.Acl.findOne({collectionName:"Teams","controlList.role":req.user.role},function(err,obj){
-      if(err)
-      res.send("you cant see this team");
-      else
-      Collections.Teams.findOne({name:req.params.teamName},function(err,teamobj){
-        res.send(teamobj);
-      });
-    });
+    res.send(Controller.getTeam(Collections,res,req.user.role,req.params.teamName));
   });
-  server.get('/organizations/:orgName',function(req,res){
-    checkLoginBlock(req,res)
-    Collections.Acl.findOne({collectionName:"Organizations","controlList.role":req.user.role},function(err,obj){
-      if(err)
-      res.send("you cant see this org");
-      else
-      Collections.Organizations.findOne({name:req.params.orgName},function(err,teamobj){
-        res.send(teamobj);
-      });
-    });
 
+  server.get('/organizations/:orgName',function(req,res){
+    checkLoginBlock(req,res);
+    res.send(Controller.getOrganization(Collections,res,req.user.role,req.params.orgName));
   });
+
   server.get('/login',passport.authenticate('local'),function(req,res){
     res.send(req.user);
   });
