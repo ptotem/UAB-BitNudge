242fb98ffc3553012eb2d3ab1a791306be8e6cf9
vandanaptotem <vandana.ptotem@gmail.com>
2014-09-23 11:29:29 +0530

    Goals Master Tags and Transaction Master Edit

--- app/system/models/TransactionMaster/TransactionMasterCollection.js
+++ app/system/models/TransactionMaster/TransactionMasterCollection.js
@@ -1,12 +1,22 @@
+
 var mongoose=require('mongoose');
 var Schema=mongoose.Schema;
+//var TransactionMasterSchema=new Schema({
+// name:String,
+// format:String,
+// noOfParams:Number,
+// pointsFn:String, //function that will be called to get points
+// onApproved:String, //function that will be called after approval
+// createdAt:Date
+//});
 var TransactionMasterSchema=new Schema({
-  name:String,
-  format:String,
-  noOfParams:Number,
-  pointsFn:String,  //function that will be called to get points
-  onApproved:String,  //function that will be called after approval
-  createdAt:Date
+    name:String,
+    message:String,
+    tpType:String,
+    noOfParams:Number,
+    pointsFn:String, //function that will be called to get points
+    createdAt:Date
 });
 var TransactionMasterCollection=mongoose.model('transactionMaster',TransactionMasterSchema);
 module.exports=TransactionMasterCollection;
+
