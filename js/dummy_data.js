8db5dad5dcfc8ed34815682d3f29fc27a39727e3
Anagha Athanikar <anagha@ptotem.com>
2014-10-13 19:00:48 +0530

    admin_panel_changes

--- js/dummy_data.js
+++ js/dummy_data.js
@@ -44,3 +44,40 @@ var top_five_leaders_JSON = {"leaders":[
         "team": "Red"
     }
 ]};
+
+
+
+
+var top_five_challenger_JSON = {"challengers":[
+    {
+        "position": 1,
+        "name": "Carol",
+        "avatar": "img/avatar/avatar_1.jpg",
+        "team": "Red"
+    },
+    {
+        "position": 2,
+        "name": "Loren",
+        "avatar": "img/avatar/avatar_3.jpg",
+        "team": "Blue"
+    },
+    {
+        "position": 3,
+        "name": "Julia",
+        "avatar": "img/avatar/avatar_5.jpg",
+        "team": "Green"
+    },
+    {
+        "position": 4,
+        "name": "Mike",
+        "avatar": "img/avatar/avatar_4.jpg",
+        "team": "Green"
+    },
+    {
+        "position": 5,
+        "name": "Billy",
+        "avatar": "img/avatar/avatar_2.jpg",
+        "team": "Red"
+    }
+]};
+
