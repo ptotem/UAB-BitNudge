1bfc0ce21fc9ca65321ab7f4f1ddaf9570a845cb
NileshPanchal <neil08.panchal@gmail.com>
2014-10-30 18:33:45 +0530

    employess thing

--- app/system/controllers/UsersController.js
+++ app/system/controllers/UsersController.js
@@ -25,7 +25,7 @@ var UsersController={
     });
   },
   getUser:function(req,res){
-    UsersModel.getUser(req.params.userId,"","",[{path:'teams',select:'name',model:'teams'},{path:'roles',model:'roles',select:'name'},{path:'orgtags',model:'orgTags',select:'name'}],function(err,obj){
+    UsersModel.getUser(req.params.userId,"","",[{path:'teams',select:'name',model:'teams'},{path:'roles',model:'roles',select:'name'},{path:'orgtags',model:'orgTags',select:'name'},{path:'reportsTo',model:"users",select:"name"}],function(err,obj){
       res.send(obj);
       // if(req.user._id==req.params.userId)
       //   res.send(obj);
