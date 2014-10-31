552090386b96cf6ea9b9946d46f16f5d9dce44aa
Anagha Athanikar <anagha@ptotem.com>
2014-10-31 13:44:03 +0530

    tab_added

--- app/web_pages/public/js/app/app_config.js
+++ app/web_pages/public/js/app/app_config.js
@@ -1,3 +1,3 @@
-remote_host = "http://chrumble.com:3019/";
-local_host = "http://chrumble.com:3019/public/";
+remote_host = "http://192.168.2.23:3004/";
+local_host = "http://192.168.2.23:3004/public/";
 admin_pages_host = "http://localhost:3004/public/admin_pages/";
