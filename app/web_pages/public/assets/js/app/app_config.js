197b0e43fdad792d265c728f8cf3b835cd75bb48
Anagha Athanikar <anagha@ptotem.com>
2014-11-10 13:22:16 +0530

    first_commit

--- app/web_pages/public/assets/js/app/app_config.js
+++ app/web_pages/public/assets/js/app/app_config.js
@@ -1,3 +1,3 @@
 remote_host = "http://192.168.2.23:3004/";
 local_host = "http://192.168.2.23:3004/public/";
-admin_pages_host = "http://localhost:3004/public/admin_pages/";
+admin_pages_host = "http://192.168.2.23:3004/public/admin_pages/";
