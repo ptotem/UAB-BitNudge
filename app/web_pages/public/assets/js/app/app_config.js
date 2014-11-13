7074823b4bc573c4727309cda76b23c7e87a498a
Anagha Athanikar <anagha@ptotem.com>
2014-11-13 15:24:09 +0530

    Search,setting,profile removed,logout function added

--- app/web_pages/public/assets/js/app/app_config.js
+++ app/web_pages/public/assets/js/app/app_config.js
@@ -1,5 +1,5 @@
-remote_host = "http://localhost:3004/";
-local_host = "http://localhost:3004/public/";
+remote_host = "http://192.168.2.23:3004/";
+local_host = "http://192.168.2.23/public/";
 admin_pages_host = "http://localhost:3004/public/admin_pages/";
 
 // for server
