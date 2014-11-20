18d305db246ea9655ac150ef3802f31bcc4e971c
vandanaptotem <vandana.ptotem@gmail.com>
2014-11-20 10:54:17 +0530

    config changes

--- app/server.js
+++ app/server.js
@@ -14,8 +14,8 @@ var TransactionMasterCollection=require('./system/models/TransactionMaster/Trans
 var EventsController=require('./system/controllers/EventsController.js');
 var bunyan = require('bunyan');
 
-//mongoose.connect('mongodb://localhost/uabTest'); //localdb
-mongoose.connect('mongodb://localhost/uabTest-SysConfig'); //serverdb - chrumble.com:3019 - db
+mongoose.connect('mongodb://localhost/uabTest'); //localdb
+//mongoose.connect('mongodb://localhost/uabTest-SysConfig'); //serverdb - chrumble.com:3019 - db
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -102,7 +102,7 @@ server.post('/login/bitnudge',passport.authenticate('local',{session:false}), fu
 
 
 //starting the server. - on chrumble.com:3019
-server.listen(3019, function () {
+server.listen(3004, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
 
--- app/web_pages/public/assets/js/app/app_config.js
+++ app/web_pages/public/assets/js/app/app_config.js
@@ -1,11 +1,11 @@
-//remote_host = "http://localhost:3004/";
-//local_host = "http://localhost:3004/public/";
-//admin_pages_host = "http://localhost:3004/public/admin_pages/";
+remote_host = "http://localhost:3004/";
+local_host = "http://localhost:3004/public/";
+admin_pages_host = "http://localhost:3004/public/admin_pages/";
 
 // for server
-remote_host = "http://chrumble.com:3019/";
-local_host = "http://chrumble.com:3019/public/";
-admin_pages_host = "http://localhost:3004/public/admin_pages/";
+//remote_host = "http://chrumble.com:3019/";
+//local_host = "http://chrumble.com:3019/public/";
+//admin_pages_host = "http://localhost:3004/public/admin_pages/";
 
 function getParameterByName(name) {
     var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
